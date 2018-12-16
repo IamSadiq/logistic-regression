9f67adaf6fe6152d3b0e1b376829832753d6731f
Abu-Bakr Adam <asiddik5@gmail.com>
2018-12-16 11:58:42 +0100

    modified process.py

--- process.py
+++ process.py
@@ -1,6 +1,24 @@
 import numpy as np 
 import pandas as pd
 
-data = pd.read_csv('./data/ecommerce_data.csv')
+def getData():
+    df = pd.read_csv('./data/ecommerce_data.csv')
+    data = df.as_matrix()
 
-print(data.head())
+    X = data[:, :-1]
+    Y = data[:, -1]
+
+    X[:, 1] = (X[:, 1] - X[:, 1].mean()) / X[:, 1].std()
+    X[:, 2] = (X[:, 2] - X[:, 2].mean()) / X[:, 2].std()
+
+    N, D = X.shape
+
+    X2 = np.zeros((N, D+3))
+    X2[:, 0:(D-1)] = X[:, 0:(D-1)]
+
+    print(X)
+    print(Y)
+    print(X2)
+
+
+# getData()
